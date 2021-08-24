94bb46909d682d31ddce4b75734d6c591188897d
gralp <62028969+gralp-1@users.noreply.github.com>
2021-08-24 19:19:44 +0100

    renaming things

---
+++ src/main/java/com/jorji/dyeablenetherite/main.java
@@ -0,0 +1,10 @@
+package com.jorji.dyeablenetherite;
+
+import net.fabricmc.api.ModInitializer;
+
+public class main implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
