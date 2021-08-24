64346d8d9bd18ee8972be35b909ed925eb3a8b9c
gralp <62028969+gralp-1@users.noreply.github.com>
2021-08-24 19:53:49 +0100

    copied over most of the mod

--- src/main/java/com/jorji/dyeablenetherite/main.java
+++ src/main/java/com/jorji/dyeablenetherite/main.java
@@ -1,10 +1,12 @@
 package com.jorji.dyeablenetherite;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.util.registry.Registry;
 
 public class main implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		System.out.println("Hello Fabric world!");
+
+
 	}
 }
