16e822807e08ba0f4e84fe9fd186302e8d328b6f
gralp <62028969+gralp-1@users.noreply.github.com>
2021-08-25 10:53:19 +0100

    started purple

---
+++ src/main/java/com/jorji/dyeablenetherite/armour/RegisterPurpleArmour.java
@@ -0,0 +1,24 @@
+package com.jorji.dyeablenetherite.armour;
+
+import com.jorji.dyeablenetherite.materials.BlueArmourMaterial;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class RegisterPurpleArmour {
+    
+    public static final Item PURPLE_NETHERITE_HELMET = new ArmorItem(BlueArmourMaterial.INSTANCE, EquipmentSlot.HEAD, new Item.Settings().group(ItemGroup.COMBAT).fireproof());
+    public static final Item PURPLE_NETHERITE_CHESTPLATE = new ArmorItem(BlueArmourMaterial.INSTANCE, EquipmentSlot.CHEST, new Item.Settings().group(ItemGroup.COMBAT).fireproof());
+    public static final Item PURPLE_NETHERITE_LEGGINGS = new ArmorItem(BlueArmourMaterial.INSTANCE, EquipmentSlot.LEGS, new Item.Settings().group(ItemGroup.COMBAT).fireproof());
+    public static final Item PURPLE_NETHERITE_BOOTS = new ArmorItem(BlueArmourMaterial.INSTANCE, EquipmentSlot.FEET, new Item.Settings().group(ItemGroup.COMBAT).fireproof());
+
+    public static void register() {
+        Registry.register(Registry.ITEM, new Identifier("dyeablenetherite", "purple_netherite_helmet"), PURPLE_NETHERITE_HELMET);
+        Registry.register(Registry.ITEM, new Identifier("dyeablenetherite", "purple_netherite_chestplate"), PURPLE_NETHERITE_CHESTPLATE);
+        Registry.register(Registry.ITEM, new Identifier("dyeablenetherite", "purple_netherite_leggings"), PURPLE_NETHERITE_LEGGINGS);
+        Registry.register(Registry.ITEM, new Identifier("dyeablenetherite", "purple_netherite_boots"), PURPLE_NETHERITE_BOOTS);
+    }
+}
---
+++ src/main/java/com/jorji/dyeablenetherite/materials/PurpleArmourMaterial.java
@@ -0,0 +1,57 @@
+package com.jorji.dyeablenetherite.materials;
+
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+public class PurpleArmourMaterial implements ArmorMaterial {
+    private static final int[] BASE_DURABILITY = new int[]{481, 555, 592, 407};
+    private static final int[] PROTECTION_VALUES = new int[]{3, 6, 8, 3};
+    // In which A is helmet, B chestplate, C leggings and D boots.
+
+
+    @Override
+    public int getDurability(EquipmentSlot slot) {
+        return BASE_DURABILITY[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getProtectionAmount(EquipmentSlot slot) {
+        return PROTECTION_VALUES[slot.getEntitySlotId()];
+    }
+
+    @Override
+    public int getEnchantability() {
+        return 15;
+    }
+
+    @Override
+    public SoundEvent getEquipSound() {
+        return SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return Ingredient.ofItems(Items.NETHERITE_INGOT);
+    }
+
+    @Override
+    public String getName() {
+        return "purple_netherite";
+    }
+
+    @Override
+    public float getToughness() {
+        return 3.0F;
+    }
+
+    @Override
+    public float getKnockbackResistance() {
+        return 0.1F;
+    }
+
+    public static final PurpleArmourMaterial INSTANCE = new PurpleArmourMaterial();
+}
